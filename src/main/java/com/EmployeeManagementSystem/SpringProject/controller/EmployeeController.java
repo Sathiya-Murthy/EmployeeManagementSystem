2d42870d3d525f9acae64c71a9bd5869f746ebe4
Sathiya-Murthy <sathiyamurthy31@gmail.com>
2022-06-27 07:29:59 +0530

    [Add] JWT authentication added

--- src/main/java/com/EmployeeManagementSystem/SpringProject/controller/EmployeeController.java
+++ src/main/java/com/EmployeeManagementSystem/SpringProject/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.EmployeeManagementSystem.SpringProject.models.Employee;
 import com.EmployeeManagementSystem.SpringProject.service.EmployeeService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -15,23 +16,26 @@ public class EmployeeController {
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
-    @PostMapping
+    @PostMapping("/add")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Employee> saveEmployee(@RequestBody Employee employee){
         return new ResponseEntity<Employee>(employeeService.saveEmployee(employee), HttpStatus.CREATED);
     }
-    @GetMapping
+    @GetMapping("/all")
     public List<Employee> getAllEmployee(){
         return (employeeService.getAllEmployee());
     }
-    @GetMapping("{id}")
+    @GetMapping("/find/{id}")
     public ResponseEntity<Employee> getEmployeeByid(@PathVariable int id){
         return new ResponseEntity<Employee>(employeeService.getEmployeeByid(id),HttpStatus.OK);
     }
-    @PutMapping("{id}")
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee ,@PathVariable int id){
         return new  ResponseEntity<Employee>(employeeService.updateEmployee(employee,id),HttpStatus.OK);
     }
-    @DeleteMapping("{id}")
+    @DeleteMapping("/delete{id}")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<String> deleteEmployee(@PathVariable int id){
         employeeService.deleteEmployee(id);
         return new ResponseEntity<String >("Employee data deleted",HttpStatus.OK);
---
+++ src/main/java/com/EmployeeManagementSystem/SpringProject/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.EmployeeManagementSystem.SpringProject.repository;
+
+import com.EmployeeManagementSystem.SpringProject.models.ERole;
+import com.EmployeeManagementSystem.SpringProject.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
